1d1c2ca38f6cd8de93441fb3ec6e96be1da137b6
juanvazquezfaza <juan.vazquez@gmx.es>
2026-03-14 13:46:07 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "filamentos-definitivo-v3";
+const CACHE = "filamentos-definitivo-v4";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./","./index.html","./styles.css","./app.js","./data/config.json","./data/inventario.json"])));
 });
