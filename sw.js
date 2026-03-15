b12f74db176eb3e47cce12a7c30ce6cb38976f73
juanvazquezfaza <juan.vazquez@gmx.es>
2026-03-15 11:15:32 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "filamentos-definitivo-v24";
+const CACHE = "filamentos-definitivo-v25";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./","./index.html","./styles.css","./app.js","./data/config.json","./data/inventario.json"])));
 });
